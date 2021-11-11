e8d0feb10fbfed434821131b3bc3ee325687f909
Rehan Raihan <68391710+raihanuddin2002@users.noreply.github.com>
2021-11-12 02:39:20 +0600

    dynamic rating added

--- src/components/Home/ShowReview/ShowReview.js
+++ src/components/Home/ShowReview/ShowReview.js
@@ -6,6 +6,7 @@ const ShowReview = (props) => {
         <div className="border-bottom border-danger mb-4">
             <h5 className="text-danger"> <img src="https://i.ibb.co/Z8gDRXm/mfp-right.png" alt="" /> {name}</h5>
             <p><small>{comment}</small></p>
+            <div>{ [...Array(rating)].map(e => <small><i className="fas fa-star fs-5 text-pink"></i></small> ) } <b className="ms-2">{rating}</b>/5</div>
         </div>
     );
 };
